d7b2cacfbfafc074294b51cf6d64d47f499b3b3e
JosephMLX <menglingxu95@gmail.com>
2018-10-18 16:45:58 +1100

    edited

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,6 @@
 from flask import Flask
 from flask_restplus import Api
-from flask_cors import CORS
-from util.DB_Interface import DB
 
 app = Flask(__name__)
-CORS(app)
+app.config['ERROR_404_HELP'] = False
 api = Api(app)
-db = DB()
--- backend/run.py
+++ backend/run.py
@@ -1,21 +1,11 @@
 import os, sys
+import pymongo
 
-database_dir = os.path.join('db')
-if not os.path.exists(database_dir):
-    print('Creating', database_dir)
-    os.mkdir(database_dir)
 try:
-    from app import app
-    import namespaces.post
-    import namespaces.auth
-    import namespaces.user
-    import namespaces.dummy
-    app.run(debug=True)
+	from app import app
+	import namespaces.user
+	app.run(debug=True)
 except ImportError as e:
-    print('ERROR:', e, file=sys.stderr)
-    if sys.version_info < (3,6):
-        print('The backend requires Python 3.6 or later - you appear to be using Python {}.{}'.format(*sys.version_info), file=sys.stderr)
-    else:
-        print('A module required by the backend is missing.', file=sys.stderr)
-        print('See the instructions in backend/README.md for installing the required modules.', file=sys.stderr)    
-        print('Ask in the forum if you can not fix this problem.', file=sys.stderr) 
+	print('ERROR:', e, file=sys.stderr)
+	if sys.version_info < (3,6):
+		print('laji')
