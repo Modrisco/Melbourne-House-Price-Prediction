e10c9f89bfba1791220e1554d3ae9896b9b62107
Alan Koo <alankoo1995@gmail.com>
2018-10-21 13:56:50 +1100

    implement login function, connecting with backend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,15 @@
+userToken = ""
+
 var submitButton = document.getElementById('submit-button');
 
 submitButton.addEventListener('click', () => {
-    var form = new FormData(document.getElementById("rawInput"));
-    window.setInterval(()=>console.log(form), 1000);
-    var token = prompt("token", "");
-    if (token == '') {
-        alert('Enter a token!')
+    if(!userToken){
+        $('#notLogin').collapse('show')
+        console.log('please login')
+        return
     }
     var houseData = {}
-    houseData.Token = token;
+    houseData.Token = userToken;
     houseData.Suburb = document.getElementById('suburb').value;
     houseData.Rooms = document.getElementById('bedrooms').value;
     houseData.Type = document.getElementById('type').value;
@@ -33,20 +34,61 @@ function getPrice(data) {
             // "Content-Type": "application/x-www-form-urlencoded",
         },
         redirect: "follow", // manual, *follow, error
-    }).catch(error => console.error('Error:', error))
+    })
     .then(res => res.json())
     .then(response => {
           console.log(response)
             var housePrice = document.getElementById('price')
-            if(response.price) housePrice.innerText = response.price
+            if(response.price){
+                housePrice.innerText = response.price
+                if(!$('#invalidResult').hasClass('d-none')) $('#invalidResult').addClass('d-none')
+                $('#result').removeClass('d-none')
+            }
             else {
-                $('#invalidResult').toggleClass('d-none')
-                $('#result').toggleClass('d-none')
+                if(!$('#result').hasClass('d-none')) $('#result').addClass('d-none')
+                $('#invalidResult').removeClass('d-none')
             }
         console.log('Success:', JSON.stringify(response))
     })
+    .catch(error => console.error('Error:', error))
     
 }
 
-//>> Alan Code
+console.log('user')
+$('#loginSubmit').click(()=>{
+    let url = 'http://127.0.0.1:5000/auth/login'
+    let data = {
+        username: $('#usernameInput').val(),
+        password: $('#passwordInput').val()
+    }
+    fetch(url,{
+        method:'POST',
+        body:JSON.stringify(data),
+        headers:{
+            "Content-Type":"application/json"
+        }
+    })
+    .then(res=>{
+        if(res.status === 400 || res.status === 403) {
+            $('#invalidLogin').removeClass('d-none')
+            throw 0
+        }
+        return res.json()
+    })
+    .then(resp=>{
+        console.log(resp.token)
+        // regex = new new RegExp(';\s?')
+        userToken = resp.token
+        $('#notLogin').collapse('hide')
+        $('#login_modal').modal('hide')
+    })
+    .catch(err=>{
+        // if(err === 0) $()
+    })
+})
 
+$('#login_modal').on('hidden.bs.modal', function (e) {
+    $('#invalidLogin').addClass('d-none')
+    $('#usernameInput').prop('value','')
+    $('#passwordInput').prop('value','')
+})
